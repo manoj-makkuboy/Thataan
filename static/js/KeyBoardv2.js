dc6f5b6b1acf81e82ee18d64f02443708b9a3520
Manoj Mohan <m.manoj@linuxmail.org>
2017-10-02 22:40:09 +0530

    Add tamil layer 0 keys on the css keyboard

--- static/js/KeyBoardv2.js
+++ static/js/KeyBoardv2.js
@@ -1,54 +1,57 @@
 import React, { Component } from 'react'
 import './KeyBoardv2.css'
 
-function getKeyId (inputLetter) {
-  let keyMap = {
-'c32': [[' '],['']],
-'c13': [['\n'],[]],
+const currentKeyboard = 'tamil'
+
+const keyMap = {
+	  'c32':{[currentKeyboard]: [[' '],['']]},
+	  'c13':{[currentKeyboard]: [['\n'],[]]},
 // qwerty row
-'c81': [['ஆ', 'ா'],['ஸ']],
-'c87':[['ஈ', 'ீ'],['ஷ']],
-'c69':[['ஊ', ' ூ'],['ஜ']],
-'c82':[['ஐ',' ை'],['ஹ']],
-'c84':[['ஏ',' ே'],['க்ஷ']],
-'c89':[['ள','', ''],['ஶ்ரீ']],
-'c85':[['ற','', ''],['ஶ']],
-'c73':[['ன', ''],[':']],
-'c79':[['ட', ''],['[']],
-'c80':[['ண', ''],[']']],
-'c219':[['ச', ''],['{']],
-'c221':[['ஞ', ''],['}']],
+'c81':{english: ['q'], tamil: [['ஆ', 'ா'],['ஸ']]},
+'c87':{tamil: [['ஈ', 'ீ'],['ஷ']]},
+'c69':{tamil: [['ஊ', ' ூ'],['ஜ']]},
+'c82':{tamil: [['ஐ',' ை'],['ஹ']]},
+'c84':{tamil: [['ஏ',' ே'],['க்ஷ']]},
+'c89':{tamil: [['ள','', ''],['ஶ்ரீ']]},
+'c85':{tamil: [['ற','', ''],['ஶ']]},
+'c73':{tamil: [['ன', ''],[':']]},
+'c79':{tamil: [['ட', ''],['[']]},
+'c80':{tamil: [['ண', ''],[']']]},
+'c219':{tamil: [['ச', ''],['{']]},
+'c221':{tamil: [['ஞ', ''],['}']]},
 	  // asdfg row 
-'c65':[['அ','',''],['௹']],
-'c83':[['இ','ி',''],['௺']],
-'c68':[['உ','ு',''],['௸']],
-'c70':[['்',''],['ஃ']],
-'c71':[['எ','ெ',''],['']],
-'c72':[['க','',''],['']],
-'c74':[['ப','',''],['']],
-'c75':[['ம','',''],['']],
-'c76':[['த','',''],['']],
-'c186':[['ந','',''],['']],
-'c222':[['ய','',''],['']],
+'c65':{tamil: [['அ','',''],['௹']]},
+'c83':{tamil: [['இ','ி',''],['௺']]},
+'c68':{tamil: [['உ','ு',''],['௸']]},
+'c70':{tamil: [['்',''],['ஃ']]},
+'c71':{tamil: [['எ','ெ',''],['']]},
+'c72':{tamil: [['க','',''],['']]},
+'c74':{tamil: [['ப','',''],['']]},
+'c75':{tamil: [['ம','',''],['']]},
+'c76':{tamil: [['த','',''],['']]},
+'c186':{tamil: [['ந','',''],['']]},
+'c222':{tamil: [['ய','',''],['']]},
 	  // zxcvb row
-         'c90':[['ஔ','ௌ',''],['௳']],
-         'c88':[['ஓ','ோ',''],['௴']],
-         'c67':[['ஒ','ொ',''],['௵']],
-         'c86':[['வ','',''],['௶']],
-         'c66':[['ங','',''],['௷']],
-         'c78':[['ல','',''],['ௐ']],
-         'c77':[['ர','',''],['/']],
-         'c188':[[',','',''],['']],
-         'c190':[['.','',''],['']],
-	  'c191':[['ழ','',''],['?']],
-         'c16' :[['','',''],['']]
+         'c90':{tamil: [['ஔ','ௌ',''],['௳']]},
+         'c88':{tamil: [['ஓ','ோ',''],['௴']]},
+         'c67':{tamil: [['ஒ','ொ',''],['௵']]},
+         'c86':{tamil: [['வ','',''],['௶']]},
+         'c66':{tamil: [['ங','',''],['௷']]},
+         'c78':{tamil: [['ல','',''],['ௐ']]},
+         'c77':{tamil: [['ர','',''],['/']]},
+         'c188':{tamil: [[',','',''],['']]},
+         'c190':{tamil: [['.','',''],['']]},
+	  'c191':{tamil: [['ழ','',''],['?']]}
+	  //   'c16' :[['','',''],['']]
 
 }
+
+function getKeyId (inputLetter, language) {
   for (let key in keyMap) {
-    if (keyMap[key][0].includes(inputLetter)) {
+    if (keyMap[key][language][0].includes(inputLetter)) {
       return key  
     }
-    if (keyMap[key][1].includes(inputLetter)) {
+    if (keyMap[key][language][1].includes(inputLetter)) {
       return key  
     }
   }
@@ -70,17 +73,18 @@ class KeyBoardv2 extends Component {
       console.log(nextProps.pressedKey)
 	    // reset old pressed key 
       try {
-        let keyToReset = window.document.getElementsByClassName(getKeyId(this.props.pressedKey))
+        let keyToReset = window.document.getElementsByClassName(getKeyId(this.props.pressedKey, currentKeyboard))
         keyToReset[0].removeAttribute('style'); 
       } catch (e) {
        console.log('no keys pressed to revert highlight') 
       }
 	    // mark new pressed key 
-      let keyIdToPress = window.document.getElementsByClassName(getKeyId(nextProps.pressedKey))
+      
+      let keyIdToPress = window.document.getElementsByClassName(getKeyId(nextProps.pressedKey, currentKeyboard))
 	  console.log('keyTopress')
 	  console.log(keyIdToPress)
       keyIdToPress[0].setAttribute('style','background: #34f3cf;'); 
-      this.setState = {keyIdToPress: getKeyId(nextProps.pressedKey)}
+      this.setState = {keyIdToPress: getKeyId(nextProps.pressedKey, currentKeyboard)}
     }
   }
 
@@ -121,47 +125,47 @@ class KeyBoardv2 extends Component {
         </ul>
     	<ul className="cf" id="qwerty">
 	    	<li><a href="#" className="key c9" id="tab"><span>tab</span></a></li>
-	    	<li><a href="#" className="key c81"><span>q</span></a></li>
-	    	<li><a href="#" className="key c87"><span>w</span></a></li>
-	    	<li><a href="#" className="key c69"><span>e</span></a></li>
-	    	<li><a href="#" className="key c82"><span>r</span></a></li>
-	    	<li><a href="#" className="key c84"><span>t</span></a></li>
-	    	<li><a href="#" className="key c89"><span>y</span></a></li>
-	    	<li><a href="#" className="key c85"><span>u</span></a></li>
-	    	<li><a href="#" className="key c73"><span>i</span></a></li>
-	    	<li><a href="#" className="key c79"><span>o</span></a></li>
-	    	<li><a href="#" className="key c80"><span>p</span></a></li>
-	    	<li><a href="#" className="key c219 alt"><b>{'{'}</b><span>[</span></a></li>
-	    	<li><a href="#" className="key c221 alt"><b>{'}'}</b><span>]</span></a></li>
+		<li><a href="#" className="key c81"><span>q</span><span>{keyMap.c81[currentKeyboard][0][0]}</span></a></li>
+	    	<li><a href="#" className="key c87"><span>w</span><span>{keyMap.c87[currentKeyboard][0][0]}</span></a></li>
+	    	<li><a href="#" className="key c69"><span>e</span><span>{keyMap.c69[currentKeyboard][0][0]}</span></a></li>
+	    	<li><a href="#" className="key c82"><span>r</span><span>{keyMap.c82[currentKeyboard][0][0]}</span></a></li>
+	    	<li><a href="#" className="key c84"><span>t</span><span>{keyMap.c84[currentKeyboard][0][0]}</span></a></li>
+	    	<li><a href="#" className="key c89"><span>y</span><span>{keyMap.c89[currentKeyboard][0][0]}</span></a></li>
+	    	<li><a href="#" className="key c85"><span>u</span><span>{keyMap.c81[currentKeyboard][0][0]}</span></a></li>
+	    	<li><a href="#" className="key c73"><span>i</span><span>{keyMap.c85[currentKeyboard][0][0]}</span></a></li>
+	    	<li><a href="#" className="key c79"><span>o</span><span>{keyMap.c79[currentKeyboard][0][0]}</span></a></li>
+	    	<li><a href="#" className="key c80"><span>p</span><span>{keyMap.c80[currentKeyboard][0][0]}</span></a></li>
+	    	<li><a href="#" className="key c219 alt"><b>{'{'}</b><span>{keyMap.c80[currentKeyboard][0][0]}</span></a></li>
+	    	<li><a href="#" className="key c221 alt"><b>{'}'}</b><span>{keyMap.c80[currentKeyboard][0][0]}</span></a></li>
 	    	<li><a href="#" className="key c220 alt"><b>|</b><span>\</span></a></li>
         </ul>
         <ul className="cf" id="asdfg">
 	    	<li><a href="#" className="key c20 alt" id="caps"><b></b><span>caps lock</span></a></li>
-	    	<li><a href="#" className="key c65"><span>a</span></a></li>
-	    	<li><a href="#" className="key c83" ><span>s</span></a></li>
-	    	<li><a href="#" className="key c68" ><span>d</span></a></li>
-	    	<li><a href="#" className="key c70"><span>f</span></a></li>
-	    	<li><a href="#" className="key c71"><span>g</span></a></li>
-	    	<li><a href="#" className="key c72"><span>h</span></a></li>
-	    	<li><a href="#" className="key c74"><span>j</span></a></li>
-	    	<li><a href="#" className="key c75"><span>k</span></a></li>
-	    	<li><a href="#" className="key c76"><span>l</span></a></li>
-	    	<li><a href="#" className="key c186 alt"><b>:</b><span>;</span></a></li>
-	    	<li><a href="#" className="key c222 alt"><b>"</b><span>'</span></a></li>
+	    	<li><a href="#" className="key c65"><span>a</span><span>{keyMap.c65[currentKeyboard][0][0]}</span></a></li>
+	    	<li><a href="#" className="key c83" ><span>s</span><span>{keyMap.c83[currentKeyboard][0][0]}</span></a></li>
+	    	<li><a href="#" className="key c68" ><span>d</span><span>{keyMap.c68[currentKeyboard][0][0]}</span></a></li>
+	    	<li><a href="#" className="key c70"><span>f</span><span>{keyMap.c70[currentKeyboard][0][0]}</span></a></li>
+	    	<li><a href="#" className="key c71"><span>g</span><span>{keyMap.c71[currentKeyboard][0][0]}</span></a></li>
+	    	<li><a href="#" className="key c72"><span>h</span><span>{keyMap.c72[currentKeyboard][0][0]}</span></a></li>
+	    	<li><a href="#" className="key c74"><span>j</span><span>{keyMap.c74[currentKeyboard][0][0]}</span></a></li>
+	    	<li><a href="#" className="key c75"><span>k</span><span>{keyMap.c75[currentKeyboard][0][0]}</span></a></li>
+	    	<li><a href="#" className="key c76"><span>l</span><span>{keyMap.c76[currentKeyboard][0][0]}</span></a></li>
+	    	<li><a href="#" className="key c186 alt"><span>;</span><span>{keyMap.c186[currentKeyboard][0][0]}</span></a></li>
+	    	<li><a href="#" className="key c222 alt"><span>'</span><span>{keyMap.c222[currentKeyboard][0][0]}</span></a></li>
 	    	<li><a href="#" className="key c13 alt" id="enter"><span>return</span></a></li>
         </ul>
         <ul className="cf" id="zxcvb">
 	    	<li><a href="#" className="key c16 shiftleft"><span>Shift</span></a></li>
-	    	<li><a href="#" className="key c90"><span>z</span></a></li>
-	    	<li><a href="#" className="key c88"><span>x</span></a></li>
-	    	<li><a href="#" className="key c67"><span>c</span></a></li>
-	    	<li><a href="#" className="key c86"><span>v</span></a></li>
-	    	<li><a href="#" className="key c66"><span>b</span></a></li>
-	    	<li><a href="#" className="key c78"><span>n</span></a></li>
-	    	<li><a href="#" className="key c77"><span>m</span></a></li>
-	    	<li><a href="#" className="key c188 alt"><b>&lt;</b><span>,</span></a></li>
-	    	<li><a href="#" className="key c190 alt"><b>&gt;</b><span>.</span></a></li>
-	    	<li><a href="#" className="key c191 alt"><b>?</b><span>/</span></a></li>
+	    	<li><a href="#" className="key c90"><span>z</span><span>{keyMap.c90[currentKeyboard][0][0]}</span></a></li>
+	    	<li><a href="#" className="key c88"><span>x</span><span>{keyMap.c88[currentKeyboard][0][0]}</span></a></li>
+	    	<li><a href="#" className="key c67"><span>c</span><span>{keyMap.c67[currentKeyboard][0][0]}</span></a></li>
+	    	<li><a href="#" className="key c86"><span>v</span><span>{keyMap.c86[currentKeyboard][0][0]}</span></a></li>
+	    	<li><a href="#" className="key c66"><span>b</span><span>{keyMap.c66[currentKeyboard][0][0]}</span></a></li>
+	    	<li><a href="#" className="key c78"><span>n</span><span>{keyMap.c78[currentKeyboard][0][0]}</span></a></li>
+	    	<li><a href="#" className="key c77"><span>m</span><span>{keyMap.c77[currentKeyboard][0][0]}</span></a></li>
+	    	<li><a href="#" className="key c188 alt"><span>{keyMap.c188[currentKeyboard][0][0]}</span></a></li>
+	    	<li><a href="#" className="key c190 alt"><span>{keyMap.c190[currentKeyboard][0][0]}</span></a></li>
+	    	<li><a href="#" className="key c191 alt"><span>{keyMap.c191[currentKeyboard][0][0]}</span></a></li>
 	    	<li><a href="#" className="key c16 shiftright"><span>Shift</span></a></li>
         </ul>
 <ul className="cf" id="bottomrow">
