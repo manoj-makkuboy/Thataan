3e2e29bde4f09b7adc1f1423f277a4ee2fd8f4a4
Manoj Mohan <m.manoj@linuxmail.org>
2017-10-05 16:34:42 +0530

    Remove explict route to /bundle.js

--- thataan_backend.py
+++ thataan_backend.py
@@ -34,9 +34,6 @@ app.config.from_envvar('THATAAN_SETTINGS', silent=True)
 def index():
     return app.send_static_file("index.html")
 
-@app.route('/bundle.js')
-def send_bundle():
-    return app.send_static_file('dist/bundle.js')
 
 def connect_db():
     """ connects to specific database """
