2e6ba335560804a908fca426c0632bbdad51b485
manoj-makkuboy <m.manoj@linuxmail.org>
2018-02-11 20:07:07 +0530

    Manoj | Remove login and signup feature

--- static/js/App.js
+++ static/js/App.js
@@ -1,47 +1,25 @@
 import React, { Component } from 'react'
-import {BrowserRouter, Route, Link} from 'react-router-dom'
+import { BrowserRouter, Route, Link } from 'react-router-dom'
 import TypingTutor from './TypingTutor'
-import Login from './Login'
-import SignUp from './SignUp'
-
-const Account = function () {  
-  if (localStorage.getItem('JWT')){
-    return ( <div> <h5> User Logged in </h5></div>)
-  }
-  else {
-    return (
-      <div>
-        <nav>
-                <Link to="/account/login"> Login </Link>
-                <Link to="/account/signup"> Sign-up </Link>
-        </nav>
-        <div>
-                <Route path="/account/login" component={Login}/>
-                <Route path="/account/signup" component={SignUp}/>
-        </div>
-      </div>
-    )
-  }
-}
 
 const MainTab = () => (
-  <div> 
+  <div>
     <nav>
       <Link to="/"> Practise </Link>
     </nav>
     <div>
-      <Route path="/" component={TypingTutor}/> 
+      <Route path="/" component={TypingTutor} />
     </div>
   </div>
-  )
+)
 
 
 class App extends Component {
-  render () {
+  render() {
     return (
       <div>
         <BrowserRouter>
-          <MainTab/>
+          <MainTab />
         </BrowserRouter>
       </div>
     )
